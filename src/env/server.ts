5267e1721439742a60e06fa7aec312511749ace5
shakibdshy <shakibdshy@gmail.com>
2024-09-20 21:51:00 +0600

    Typesafe env setup

---
+++ src/env/server.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { ZodError, z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    DATABASE_URL: z.string().url(),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  experimental__runtimeEnv: process.env,
+});
