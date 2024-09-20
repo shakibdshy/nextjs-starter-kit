b6d949264bfd6e623e1c15d25d8fabde38cb5f53
shakibdshy <shakibdshy@gmail.com>
2024-09-20 19:02:44 +0600

    Nextui Setup

--- src/components/example-thing.tsx
+++
@@ -1,3 +0,0 @@
-export default function ExampleThing() {
-  return <div>ExampleThing</div>;
-}
---
+++ src/components/provider.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Provider({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
